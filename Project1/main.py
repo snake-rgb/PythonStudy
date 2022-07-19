e239a82407a3c6e1905a1ac093b2317a92bb73d4
Viktor <viktor13851998@gmail.com>
2022-07-19 17:27:50 +0300

    first commit

--- Project1/main.py
+++ Project1/main.py
@@ -1,78 +1,40 @@
 import requests
-from bs4 import BeautifulSoup
+import json
 
-url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3' \
-      '%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2'
+requestCount = 0
+failedJoke = 0
+niceJoke = 0
+jokesCounter = 0
+person_dict = {
+    "Jokes": {},
+    "Results": {
+        "Requests": 0,
+        "Saved jokes": 0,
+        "Failed jokes": 0,
+        "Nice jokes": 0,
+    }
+}
 
-request = requests.get(url)
+for i in range(1, 101):
+    res = requests.get('https://v2.jokeapi.dev/joke/Any?type=single')
+    if res.ok:
+        requestCount = requestCount + 1
+        req = res.json()
+        print(req['joke'])
+        print('________________________________')
 
-list_countries = {0: {'country_name': '',
-                      'full_country_name': '',
-                      'same_letter_count': 0,
-                      'flag_url': '',
-                      'words_full_country_name': 0
-                      }
-                  }
+    if len(req['joke']) < 60 and req['joke'].find('code') == -1:
+        failedJoke = failedJoke + 1
+    else:
+        niceJoke = niceJoke + 1
+        jokesCounter = jokesCounter + 1
+        person_dict['Jokes']['joke_' + str(jokesCounter)] = req['joke']
+        with open("data_file.json", "w") as write_file:
+            json.dump(person_dict, write_file)
 
-
-def main():
-    soup = BeautifulSoup(request.text, "html.parser")
-    countries_url = soup.find("table", class_="wikitable").findAll("img")
-    table_fields = soup.find("table", class_="wikitable").findAll("tr")
-
-    # Удаляем поле с наименованиям столбцов таблицы
-    del table_fields[0]
-
-    for i in range(0, len(table_fields)):
-        # Создаем нужное количество элементов словаря расчитаное на количество стран в таблице
-        list_countries[i] = {}
-        # Берем полное название страны из столбца №4
-        list_countries[i]["full_country_name"] = table_fields[i].find_all("td")[3].text
-        # Делим строку с полным названием на слова
-        word_count = list_countries[i]["full_country_name"].split()
-        # Записываем количество слов
-        list_countries[i]["words_full_country_name"] = len(word_count)
-        # Короткое имя
-        list_countries[i]["country_name"] = table_fields[i].find_all("td")[2].text
-
-    # Считаем количество странн начинающихся на одну и ту же букву
-    for i in range(0, len(table_fields)):
-        counter = 0
-        for j in range(0, len(table_fields)):
-            # Преобразуем буквы к одному регистру и сравниваем их
-            first_country_name = list_countries[i]["country_name"].lower()
-            second_country_name = list_countries[j]["country_name"].lower()
-            # Если буквы совпали увеличиваем счетчик
-            if first_country_name[0] == second_country_name[0]:
-                counter += 1
-        # Записываем в словарь полученные данные
-        list_countries[i]["same_letter_count"] = counter
-
-    for i in range(0, len(countries_url)):
-        list_countries[i]["flag_url"] = countries_url[i].get("src")
-
-
-# Функция для полученния данных конкретной странны по ее короткому имени
-def find_country(country_name):
-    country_name = country_name.lower()
-    i = 0
-
-    while i < len(list_countries):
-        # Убираем лишний символ переноса строки
-        buffer = list_countries[i]["country_name"].lower().replace("\n", "")
-
-        # Сравниваем именна стран при совпадении выводим полную информацию о стране
-        if country_name == buffer:
-            print(list_countries[i]["country_name"].replace("\n", ""))
-            print(list_countries[i]["full_country_name"].replace("\n", ""))
-            print(list_countries[i]["same_letter_count"])
-            print(list_countries[i]["words_full_country_name"])
-            print(list_countries[i]["flag_url"])
-            print("\n")
-        i += 1
-
-
-if __name__ == '__main__':
-    main()
-    find_country("Австралия")
-    find_country("Украина")
+person_dict['Results']['Requests'] = requestCount
+person_dict['Results']['Saved jokes'] = niceJoke
+person_dict['Results']['Failed jokes'] = failedJoke
+person_dict['Results']['Nice jokes'] = niceJoke
+with open("data_file.json", "w") as write_file:
+    json.dump(person_dict, write_file, indent=4, sort_keys=False)
