62a9e079454cd4e20dc006ad8bde4d63989cd03e
Viktor <viktor13851998@gmail.com>
2022-07-21 18:36:22 +0300

    Fixed

--- Project4/additional.py
+++ Project4/additional.py
@@ -18,14 +18,25 @@ def schedule2(date_start="2022-04-20", date_end="2022-04-23", days_work=1, days_
                 if j <= 0:
                     break
                 if data[1] > 0:
-
-                    for k in itertools.count(0, 1):
-                        if k < data[2]:
+                    if data[1] != 1:
+                        for k in itertools.count(0, 1):
+                            if k < data[2]:
+                                data[3] += timedelta(k)
+                                date_work.append(data[3])
+                            else:
+                                break
+                    else:
+                        if len(date_work) >= 1:
                             data[3] += timedelta(1)
                             date_work.append(data[3])
                         else:
-                            break
-                    data[3] += timedelta(days_skip)
-                    data[0] -= days_work + days_skip
+                            data[3] += timedelta(0)
+                            date_work.append(data[3])
 
+                data[3] += timedelta(days_skip)
+                data[0] -= days_work + days_skip
+
+    if days_skip == 0:
+        d0 = date_work[-1] + timedelta(1)
+        date_work.append(d0)
     return date_work
--- Project4/main.py
+++ Project4/main.py
@@ -19,29 +19,40 @@ def schedule(date_start="2022-04-20", date_end="2022-04-23", days_work=1, days_s
 
     while days > 0 and days_work > 0:
 
-        for i in itertools.count(0, 1):
-            if i < days_work:
+        if days_work != 1:
+            for i in itertools.count(0, 1):
+                if i < days_work:
+                    d0 += timedelta(i)
+                    date_work.append(d0)
+                else:
+                    break
+        else:
+            if len(date_work) >= 1:
                 d0 += timedelta(1)
                 date_work.append(d0)
             else:
-                break
+                d0 += timedelta(0)
+                date_work.append(d0)
 
         d0 += timedelta(days_skip)
         days -= days_work + days_skip
 
+    if days_skip == 0:
+        d0 += timedelta(1)
+        date_work.append(d0)
     # print(f"{len(date_work)} - Количество рабочих дней")
     # for i in range(len(date_work)):
-    #     print(date_work[i])
+    # print(date_work[i])
     return date_work
 
 
 def main():
-    print(f"""1 schedule - {schedule("2022-04-20", "2022-04-23", 1, 1)}""")
+    print(f"""1 schedule - {schedule("2022-04-20", "2022-04-23", 1, 0)}""")
     print(f"""2 schedule - {schedule("2022-04-25", "2022-06-26", 0, 4)}""")
     print(f"""3 schedule - {schedule("2022-05-14", "2022-05-24", 1, 95)}""")
     print(f"""4 schedule - {schedule("2022-05-14", "2022-05-30", 5, 2)}""")
     print(f"""5 schedule - {schedule("2022-05-14", "2022-06-24", 2, 5)}""")
-    print(f"""6 schedule - {schedule2("2022-03-12", "2022-03-24", 2, 2)}""")
+    print(f"""6 schedule - {schedule2("2022-03-12", "2022-03-24", 1, 0)}""")
 
 
 if __name__ == '__main__':
